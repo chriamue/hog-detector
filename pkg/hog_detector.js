399da9dc972048c200a18d6f0b558b5cf5e6097a
chriamue <chriamue@users.noreply.github.com>
2022-11-27 16:09:48 +0000

    deploy: 06ca8c8568477f8b1da30c2bb8a0b3ae63c21c5f

--- pkg/hog_detector.js
+++ pkg/hog_detector.js
@@ -1263,12 +1263,12 @@ function getImports() {
         const ret = wasm.memory;
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper1583 = function(arg0, arg1, arg2) {
-        const ret = makeClosure(arg0, arg1, 626, __wbg_adapter_32);
+    imports.wbg.__wbindgen_closure_wrapper1580 = function(arg0, arg1, arg2) {
+        const ret = makeClosure(arg0, arg1, 628, __wbg_adapter_32);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper1693 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 660, __wbg_adapter_35);
+    imports.wbg.__wbindgen_closure_wrapper1690 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 662, __wbg_adapter_35);
         return addHeapObject(ret);
     };
 
