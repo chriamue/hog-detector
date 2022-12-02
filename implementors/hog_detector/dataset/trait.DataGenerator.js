18b71fc5e8ce7df97a8c897c14aa5696827f5f1c
chriamue <chriamue@users.noreply.github.com>
2022-12-02 07:49:39 +0000

    deploy: b0fce5c1ba3e5ede817d9092954cba9c221c7cb6

---
+++ implementors/hog_detector/dataset/trait.DataGenerator.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"hog_detector":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
