cffc4ae9155d11f716f69e5d8b5185ae57d4d8ca
chriamue <chriamue@users.noreply.github.com>
2022-12-23 11:14:30 +0000

    deploy: 3a5d4ab9a4e5a77571c919b3717eff59cf19efd1

--- pkg/hog_detector.d.ts
+++ pkg/hog_detector.d.ts
@@ -162,9 +162,9 @@ export interface InitOutput {
   readonly __wbgtest_console_error: (a: number) => void;
   readonly __wbg_annotatedimage_free: (a: number) => void;
   readonly annotatedimage_constructor: () => number;
+  readonly init_label_tool: (a: number, b: number, c: number, d: number) => number;
   readonly __wbg_labeltool_free: (a: number) => void;
   readonly labeltool_new: () => number;
-  readonly init_label_tool: (a: number, b: number, c: number, d: number) => number;
   readonly __wbindgen_malloc: (a: number) => number;
   readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
   readonly __wbindgen_export_2: WebAssembly.Table;
