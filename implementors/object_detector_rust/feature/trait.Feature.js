0c277df03e8faab54575f8382f7b103b093d7af8
chriamue <chriamue@users.noreply.github.com>
2023-01-28 15:44:12 +0000

    deploy: 2c5e46c91ad395ffeb107a15537d94632cb02d43

---
+++ implementors/object_detector_rust/feature/trait.Feature.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"hog_detector":[["impl Feature for <a class=\"struct\" href=\"hog_detector/feature_descriptor/struct.CombinedFeatureDescriptor.html\" title=\"struct hog_detector::feature_descriptor::CombinedFeatureDescriptor\">CombinedFeatureDescriptor</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
