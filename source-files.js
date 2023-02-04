779a9ace972344707d1f0112074fbc4cd6c892be
chriamue <chriamue@users.noreply.github.com>
2023-02-04 11:43:36 +0000

    deploy: 1af4ab44518143b976938783e46da17b150091ce

--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"hog_detector":["",[["classifier",[],["mod.rs"]],["dataset",[],["eyes_dataset.rs","mnist_dataset.rs","mod.rs"]],["feature_descriptor",[],["combined_feature_descriptor.rs","mod.rs"]]],["data_augmentation.rs","detector.rs","hogdetector.rs","lib.rs","utils.rs"]]\
+"hog_detector":["",[["classifier",[],["bayes.rs","mod.rs"]],["dataset",[],["eyes_dataset.rs","mnist_dataset.rs","mod.rs"]],["feature_descriptor",[],["combined_feature_descriptor.rs","mod.rs"]]],["data_augmentation.rs","detector.rs","hogdetector.rs","lib.rs","utils.rs"]]\
 }');
 createSourceSidebar();
