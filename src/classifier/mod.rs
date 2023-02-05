21706f53f2a64cc4d44475c304f1822e6eac3b37
Christian M <chriamue@gmail.com>
2023-02-05 10:38:05 +0100

    :fire: removed unused combined classifier

--- src/classifier/mod.rs
+++ src/classifier/mod.rs
@@ -1,7 +1,4 @@
 /// naive bayes classifier module
 pub mod bayes;
 pub use bayes::BayesClassifier;
-/// combined classifier of two classifiers
-// pub mod combined;
-// pub use combined::CombinedClassifier;
 pub use object_detector_rust::prelude::Classifier;
